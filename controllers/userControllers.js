691a830ddd6732fd2f3f558cd2339b0d8bb24fab
Adam Olson <adamlsn@gmail.com>
2021-07-03 20:09:00 -0500

    working version of User routes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,19 +1,105 @@
 const { User } = require("../models");
+const { params } = require("../routes");
 
 const userControllers = {
     getAllUsers(req, res) {
         User.find({})
-            .populate({
-                path: "user",
-                select: "-__v"
-            })
-            .select("-__v")
-            .sort({ _id: -1})
             .then(dbData => res.json(dbData))
             .catch(err => {
                 console.log(err);
                 res.sendStatus(400);
             })
+    },
+
+    getUserById({ params }, res){
+        User.findOne({ _id: params.userId })
+            .then(dbData => {
+                if(!dbData){
+                    res.status(404).json({message: "No user found with this ID"});
+                    return;
+                }
+                res.json(dbData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    createUser({ body }, res){
+        User.create(body)
+            .then(dbData => res.json(dbData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+
+    updateUser({ params, body }, res){
+        User.findOneAndUpdate({ _id: params.userId }, body, { new: true, runValidators: true })
+            .then(dbData => {
+                if(!dbData){
+                    res.status(404).json({ message: "No user found with this ID"});
+                    return;
+                }
+                res.json(dbData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+
+    deleteUser({ params }, res){
+        User.findOneAndDelete({ _id: params.userId})
+            .then(dbData => {
+                if(!dbData){
+                    res.status(404).json({ message: "No user found with this ID"})
+                }
+                res.json(dbData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+
+    createFriend({ params }, res){
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: {friends: params.friendId }},
+            { new: true, runValidators: true }
+        )
+            .then(dbData =>{
+                if(!dbData){
+                    res.statud(404).json({ message: "No user found with this ID" });
+                    return;
+                }
+                res.json(dbData);
+            })
+            .catch(err =>{
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+
+    deleteFriend({ params }, res){
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true }
+        )
+            .then(dbData =>{
+                if(!dbData){
+                    res.status(404).json({ message: "No user found with this ID" });
+                    return;
+                }
+                res.json(dbData);
+            })
+            .catch(err =>{
+                console.log(err);
+                res.status(400).json(err);
+            });
     }
 }
 
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,29 @@
 const router = require("express").Router();
 const {
-    getAllUsers
+    getAllUsers,
+    createUser,
+    getUserById,
+    updateUser,
+    createFriend,
+    deleteFriend,
+    deleteUser
 }
 = require("../../controllers/userControllers");
 
 router
     .route("/")
     .get(getAllUsers)
+    .post(createUser);
 
+router
+    .route("/:userId")
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router
+    .route("/:userId/friends/:friendId")
+    .post(createFriend)
+    .delete(deleteFriend);
+    
 module.exports = router;
